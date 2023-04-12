d3ab75578fa366f292d644bb3f242cf76f09820a
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-12 15:33:26 +0530

    [Ritesh]add : Array Print Program

---
+++ ArrayDemo/ArrayPrint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    public class ArrayPrint
+    {
+        public void Array()
+        {
+            Console.WriteLine("Enter The Size Of Array");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[size];
+            int[] arr = Array;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter The Element Of Array");
+                int data = Convert.ToInt32(Console.ReadLine());
+                arr[i] = data;  
+            }
+            Console.WriteLine("Your Array Elements Are Given Below");
+            foreach(int item in arr)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ ArrayDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Print The Array Element");
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    ArrayPrint arrayPrint = new ArrayPrint();
+                    arrayPrint.Array();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
